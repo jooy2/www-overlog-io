54a0fc0f0e37defaa49bc32e2256a71046dac79c
leejooy96 <leejooy96@gmail.com>
2019-09-24 15:32:09 +0900

    change url

--- include/config.php
+++ include/config.php
@@ -4,7 +4,7 @@
 	require_once("functions.php");
 	
 	// Site common variables
-	define('SITE_HOME', 'http://52.78.192.102');
-	define('SITE_IMG', 'http://52.78.192.102/files/images');
+	define('SITE_HOME', 'https://overlog.io');
+	define('SITE_IMG', 'https://overlog.io/files/images');
 
 	define('SET_DEFAULT', '0');
